4cafbf7b5fee0bc0b0e301691afca2c6f81afc96
Jessica-ovabor <ovaborjessica85@gmail.com>
2023-07-11 12:50:31 +0100

    added electronics

--- src/app/consts.py
+++ src/app/consts.py
@@ -1,3 +1,46 @@
-CAR_BRAND = (
-    
+
+CARS_BRANDS = (
+    ('bmw', 'BMW'),
+    ('mercedes benz', 'Mercedes Benz'),
+    ('audi', 'Audi'),
+    ('subaru', 'Subaru'),
+    ('tesla', 'Tesla'),
+    ('jaguar', 'Jaguar'),
+    ('land rover', 'Land Rover'),
+    ('bentley', 'Bentley'),
+    ('bugatti', 'Bugatti'),
+    ('ferrari', 'Ferrari'),
+    ('lamborghini', 'Lamborghini'),
+    ('honda', 'Honda'),
+    ('toyota', 'Toyota'),
+    ('chevrolet', 'Chevrolet'),
+    ('porsche', 'Porsche')
 )
+PHONE_BRANDS = (
+    ('redmi', 'Redmi'),
+    ('samsung', 'Samsung'),
+    ('itel', 'Itel'),
+    ('motorola', 'Motorola'),
+    ('iphone', 'Iphone'),
+    ('umidigi', 'Umidigi'),
+    ('goole pixel', 'Google Pixel'),
+    ('infinix', 'Infinix'),
+    ('tecno', 'Tecno'),
+    ('xaiomi', 'Xaiomi'),
+    ('oppo', 'Oppo'),
+    ('vivo', 'Vivo'),
+    ('blackberry', 'BlackBerry'),
+    ('nokia', 'Nokia'),
+    ('poco', 'Poco')
+)
+ELECTRONIC_BRANDS = (
+    ('lg', 'LG'),
+    ('samsung', 'Samsung'),
+    ('sonic', 'Sonic'),
+    ('thermacool', 'Thermacool'),
+   
+)
+# TRANSMISSION_OPTIONS = (
+#     ('automatic', 'Automatic'),
+#     ('manual', 'Manual'),
+# )
