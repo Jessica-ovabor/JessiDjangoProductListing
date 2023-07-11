416ade1dc2baac2f790a2f6997385856fa62303b
Jessica-ovabor <ovaborjessica85@gmail.com>
2023-07-11 12:16:40 +0100

    register logout view

--- src/users/urls.py
+++ src/users/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 
-from .views import login_view , RegisterView
+from .views import login_view , RegisterView , logout_view
 
 urlpatterns =[
     path('login/',login_view, name='login'),
     path('register/',RegisterView.as_view(), name='register'),
+    path('logout/',logout_view, name='logout' )
 ]
