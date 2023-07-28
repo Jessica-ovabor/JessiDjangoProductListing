b4f3265bbbbbf3812ea34ad40458dda533d617bc
Jessica-ovabor <ovaborjessica85@gmail.com>
2023-07-28 14:37:26 +0100

    admin

--- src/app/admin.py
+++ src/app/admin.py
@@ -4,6 +4,6 @@ from django.contrib import admin
 from .models import Listing
 
 class ListingAdmin(admin.ModelAdmin):
-    readonly_fields = ('id')
+    readonly_fields = ('id',)
     
 admin.site.register(Listing, ListingAdmin)
